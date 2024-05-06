3eefe3017842a6d1802013da42e7c40e0a568497
Peter Muraus <peter.muraus@inova.si>
2024-05-06 13:54:31 +0200

    test

--- Package.swift
+++ Package.swift
@@ -14,7 +14,22 @@ let package = Package(
             targets: ["CommonTca"]),
         .library(
             name: "CommonModels",
-            targets: ["CommonModels"])
+            targets: ["CommonModels"]),
+        .library(
+            name: "FeatureExercise",
+            targets: ["FeatureExercise"]),
+        .library(
+            name: "FeatureRecorder",
+            targets: ["FeatureRecorder"]),
+        .library(
+            name: "FeatureTime",
+            targets: ["FeatureTime"]),
+        .library(
+            name: "FeatureVideo",
+            targets: ["FeatureVideo"]),
+        .library(
+            name: "FeatureWorkout",
+            targets: ["FeatureWorkout"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,6 +38,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: "CommonTca", path: "SDK/CommonTca.xcframework"),
-        .binaryTarget(name: "CommonModels", path: "SDK/CommonModels.xcframework")
+        .binaryTarget(name: "CommonModels", path: "SDK/CommonModels.xcframework"),
+        .binaryTarget(name: "FeatureExercise", path: "SDK/FeatureExercise.xcframework"),
+        .binaryTarget(name: "FeatureRecorder", path: "SDK/FeatureRecorder.xcframework"),
+        .binaryTarget(name: "FeatureTime", path: "SDK/FeatureTime.xcframework"),
+        .binaryTarget(name: "FeatureVideo", path: "SDK/FeatureVideo.xcframework"),
+        .binaryTarget(name: "FeatureWorkout", path: "SDK/FeatureWorkout.xcframework")
     ]
 )
