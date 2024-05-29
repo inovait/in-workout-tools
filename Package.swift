d4dcc63f13b65fe04ab4d8ca821e774f45c9b45b
Peter Muraus <peter.muraus@inova.si>
2024-05-29 08:36:50 +0200

    librarys fix

--- Package.swift
+++ Package.swift
@@ -10,27 +10,11 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "CommonTca",
-            targets: ["CommonTca"]),
-        .library(
             name: "CommonModels",
             targets: ["CommonModels"]),
         .library(
-            name: "FeatureExercise",
-            targets: ["FeatureExercise"]),
-        .library(
-            name: "FeatureRecorder",
-            targets: ["FeatureRecorder"]),
-        .library(
-            name: "FeatureTime",
-            targets: ["FeatureTime"]),
-        .library(
-            name: "FeatureVideo",
-            targets: ["FeatureVideo"]
-            ),
-        .library(
-            name: "FeatureWorkout",
-            targets: ["FeatureWorkout"])
+            name: "WorkoutFeature",
+            targets: ["WorkoutFeature"])        
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -38,12 +22,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "CommonTca", path: "SDK/CommonTca.xcframework"),
         .binaryTarget(name: "CommonModels", path: "SDK/CommonModels.xcframework"),
-        .binaryTarget(name: "FeatureExercise", path: "SDK/FeatureExercise.xcframework"),
-        .binaryTarget(name: "FeatureRecorder", path: "SDK/FeatureRecorder.xcframework"),
-        .binaryTarget(name: "FeatureTime", path: "SDK/FeatureTime.xcframework"),
-        .binaryTarget(name: "FeatureVideo", path: "SDK/FeatureVideo.xcframework"),
-        .binaryTarget(name: "FeatureWorkout", path: "SDK/FeatureWorkout.xcframework")
+        .binaryTarget(name: "WorkoutFeature", path: "SDK/WorkoutFeature.xcframework")
     ]
 )
